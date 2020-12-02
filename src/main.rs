67ddefd61423ec96d9a74b354ed10bde373b7822
Gunnsteinn Þórisson <gussi@gussi.is>
2020-12-02 23:20:26 +0000

    Solved day 2

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,13 @@ fn main() {
                 _ => println!("Part {} not found!", part),
             }
         },
+        2 => {
+            match part {
+                1 => println!("part 1 answer: {}", aoc2020::day2::part1().unwrap()),
+                2 => println!("part 2 answer: {}", aoc2020::day2::part2().unwrap()),
+                _ => println!("Part {} not found!", part),
+            }
+        },
         _ => println!("Day {} not found!", day),
     }
 
@@ -99,6 +106,99 @@ mod aoc2020 {
             }
 
             set
+        }
+    }
+
+    pub mod day2 {
+        use std::vec::Vec;
+        use regex::Regex;
+
+        struct PasswordPolicy {
+            a: usize,
+            b: usize,
+            chr: char,
+            passwd: String,
+        }
+
+        impl PasswordPolicy {
+            fn valid_first_rule(&self) -> bool {
+                let count = self.passwd
+                    .matches(self.chr)
+                    .count();
+
+                let min = self.a;
+                let max = self.b;
+
+                min <= count && count <= max
+            }
+
+            fn valid_second_rule(&self) -> bool {
+                let mut passwd = self.passwd.chars();
+
+                let a = passwd.nth(self.a) == Some(self.chr);
+                let b = passwd.nth(self.b) == Some(self.chr);
+
+                a ^ b
+            }
+        }
+
+        pub fn part1() -> Result<i32, &'static str> {
+            let mut valid = 0;
+
+            let input = input();
+
+            for policy in &input {
+                if policy.valid_first_rule() {
+                    valid += 1;
+                }
+            }
+
+            Ok(valid)
+        }
+
+        pub fn part2() -> Result<i32, &'static str> {
+            let mut valid = 0;
+
+            let input = input();
+
+            for policy in &input {
+                if policy.valid_second_rule() {
+                    valid += 1;
+                }
+            }
+
+            Ok(valid)
+        }
+
+        fn input() -> Vec<PasswordPolicy> {
+            let mut vec: Vec<PasswordPolicy> = Vec::new();
+
+            let re = Regex::new(r"(\d+)-(\d+) (\w): (.+)").unwrap();
+
+            loop {
+                let mut input = String::new();
+
+                std::io::stdin()
+                    .read_line(&mut input)
+                    .unwrap();
+
+                if input.trim().is_empty() {
+                    break;
+                }
+
+                let caps = re.captures(&input).unwrap();
+
+                let policy = PasswordPolicy {
+                    a       : caps.get(1).unwrap().as_str().parse().unwrap(),
+                    b       : caps.get(2).unwrap().as_str().parse().unwrap(),
+                    chr     : caps.get(3).unwrap().as_str().parse().unwrap(),
+                    passwd  : caps.get(4).unwrap().as_str().parse().unwrap(),
+                };
+
+                vec.push(policy);
+            }
+
+            vec
         }
     }
 }
